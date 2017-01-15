d9ece30d068daebd2b5beb8dd7dc933cd19a931c
jwisneski <jawisneski@gmail.com>
2017-01-15 16:24:35 -0600

    Firefox hot fix - attempt 3

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     var browser = navigator.userAgent;
 
     //Customize Copy Message
-    if(browser.includes('Firefox')){
+    if(navigator.userAgent.search("Firefox")){
         $('.container').addClass('hidden');
         $('nav').addClass('hidden');
 
