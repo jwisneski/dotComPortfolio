3dc8bd01673adf4753736455e4d230a224bd0e6f
Joel Wisneski <jawisneski@gmail.com>
2018-01-15 11:07:41 -0500

    Removing old JS files

--- js/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('a').on('click tap', function (event) {
-        $('.active').removeClass('active');
-        $(this).addClass('active');
-
-        console.log(event);
-    });
-});
